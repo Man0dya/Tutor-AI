3c66243bde4daf540bc5e05200935ba27e077c7b
KULANJANA <kulanjana2002@gmail.com>
2025-10-23 14:40:44 +0530

    update pricing.tsx

--- client/src/pages/Pricing.tsx
+++ client/src/pages/Pricing.tsx
@@ -2,6 +2,8 @@ import { Box, Container } from '@chakra-ui/react'
 import Navbar from '../components/Navbar'
 import PricingPlans from '../components/PricingPlans'
 
+
+
 export default function PricingPage() {
   return (
     <Box bg="bg" minH="100vh">
